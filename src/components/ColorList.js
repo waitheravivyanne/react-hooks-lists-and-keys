82661009caff172263be911acf3deae26b86b26e
vivian waithera <vivyannekori@gmail.com>
2023-07-19 17:11:30 +0300

    Completed assignment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -7,20 +7,26 @@ function ColorList() {
     "salmon",
     "darkslategray",
     "hotpink",
+    "mauve"
   ];
 
+
+
+const colorElements = colors.map((color) => {
   return (
-    <div>
-      <h1>Top 5 CSS Colors</h1>
-      <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
-      </ol>
-    </div>
+  <li style={{ color: color }}>{color}
+  </li>
   );
+});
+return (
+  <div>
+    <h1>Top 5 CSS Colors</h1>
+    <ol>
+      {/* display the array of <li> elements here! */}
+      {colorElements}
+    </ol>
+  </div>
+);
 }
 
 export default ColorList;
